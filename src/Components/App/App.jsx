268a4bc5047041b6895940d21210f6ac7ab77992
Brany Suero <branysuero@gmail.com>
2021-11-28 23:44:38 -0500

    added a note component

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -5,11 +5,13 @@ import React from 'react';
 
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import Note from '../Notes/Note';
 
 function App() {
   return (
     <div className="App">
       <Header />
+			<Note />
 			<Footer />
     </div>
   );

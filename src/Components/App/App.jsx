1f423872a43314397fb4cb58d3f91234b780115a
Brany Suero <branysuero@gmail.com>
2021-11-28 23:29:40 -0500

    added a footer

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,6 +1,8 @@
 
 import './App.css';
 
+import react from 'react';
+
 function App() {
   return (
     <div className="App">
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,7 @@
+import react from 'react';
+
+function Header(){
+
+}
+
+export default Header;

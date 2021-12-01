951f374f81c4c11166f8d55b9ae07ff5880dc1bf
Brany Suero <branysuero@gmail.com>
2021-11-30 23:30:10 -0500

    added multiple notes

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -7,11 +7,23 @@ import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Note from '../Notes/Note';
 
+import notes from "../notes";
+
+function addNotes(notes){
+	return (
+	<Note
+		key= {notes.key}
+		title={notes.title}
+		content={notes.content}
+		/>
+	);
+}
+
 function App() {
   return (
     <div className="App">
       <Header />
-			<Note />
+			{notes.map(addNotes)}
 			<Footer />
     </div>
   );
